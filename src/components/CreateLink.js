0ffb40c8c50c53393d50c006cbeed64274f7ad2a
Vy Nguyen <vyn@metropolia.fi>
2020-05-06 14:18:24 +0300

    finish the tutorial

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
-import { Mutation } from 'react-apollo'
-import gql from 'graphql-tag'
-import { FEED_QUERY } from './LinkList'
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import { FEED_QUERY } from "./LinkList";
+import { LINKS_PER_PAGE } from '../constants'
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -12,51 +13,58 @@ const POST_MUTATION = gql`
       description
     }
   }
-`
+`;
 class CreateLink extends Component {
   state = {
-    description: '',
-    url: '',
-  }
+    description: "",
+    url: "",
+  };
 
   render() {
-    const { description, url } = this.state
+    const { description, url } = this.state;
     return (
       <div>
         <div className="flex flex-column mt3">
           <input
             className="mb2"
             value={description}
-            onChange={e => this.setState({ description: e.target.value })}
+            onChange={(e) => this.setState({ description: e.target.value })}
             type="text"
             placeholder="A description for the link"
           />
           <input
             className="mb2"
             value={url}
-            onChange={e => this.setState({ url: e.target.value })}
+            onChange={(e) => this.setState({ url: e.target.value })}
             type="text"
             placeholder="The URL for the link"
           />
         </div>
-        <Mutation 
-          mutation={POST_MUTATION} 
+        <Mutation
+          mutation={POST_MUTATION}
           variables={{ description, url }}
-          onCompleted={()=> this.props.history.push('/')}
+          onCompleted={() => this.props.history.push("/new/1")}
           update={(store, { data: { post } }) => {
-            const data = store.readQuery({ query: FEED_QUERY })
-            data.feed.links.unshift(post)
+            const first = LINKS_PER_PAGE;
+            const skip = 0;
+            const orderBy = "createdAt_DESC";
+            const data = store.readQuery({
+              query: FEED_QUERY,
+              variables: { first, skip, orderBy },
+            });
+            data.feed.links.unshift(post);
             store.writeQuery({
               query: FEED_QUERY,
-              data
-            })
+              data,
+              variables: { first, skip, orderBy },
+            });
           }}
         >
-          {postMutation => <button onClick={postMutation}>Submit</button>}
+          {(postMutation) => <button onClick={postMutation}>Submit</button>}
         </Mutation>
       </div>
-    )
+    );
   }
 }
 
-export default CreateLink
+export default CreateLink;
